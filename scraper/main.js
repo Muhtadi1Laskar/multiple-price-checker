1f3959d6fd57c8d1e2ace0258dab6affbf31a09e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-09-19 19:48:38 +0600

    Restructured the main scraping function

--- scraper/main.js
+++ scraper/main.js
@@ -4,12 +4,18 @@ import { getBookInfo, searchBook } from "./crawler/http.js";
 // const URL = "https://www.rokomari.com/book/340423/the-master-and-margarita";
 const URL = "https://www.rokomari.com/book/467921/white-nights";
 
-(async () => {
+const scrapeMultipleData = async (URL) => {
     try {
         const bookInfo = await extractMainData(URL);
-        const result = await getBookInfo(bookInfo);
-        console.log(JSON.stringify(result, null, 2));
-    } catch (error) {
-        console.error("Failed to fetch data: ", error);
+        const scrapedData = await getBookInfo(bookInfo);
+        return scrapedData;
+    } catch(error) {
+        console.error("Failed to scrape data", error);
+        return error;
     }
+}
+
+(async () => {
+    const data = await scrapeMultipleData(URL);
+    console.log(JSON.stringify(data, null, 2));
 })();
